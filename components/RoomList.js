4bdaac96b44cca5afe14d2a501fc5d8feb283ea9
Marcelo Morais <marcelo.morais@symbio.com>
2018-09-28 10:21:14 +0200

    Updating RoomList to show data according to the response from the web request
    Signed-off-by: Marcelo Morais <marcelo.morais@symbio.com>

--- components/RoomList.js
+++ components/RoomList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import {
   StyleSheet,
   Text,
@@ -7,17 +9,39 @@ import {
   Button,
   TouchableHighlight,
   Image,
-  Alert
+  FlatList
 } from 'react-native';
 
-export default class RoomList extends Component {
+const mapStateToProps = (state) => {
+  console.log('rooms = ', state.auth.rooms);
+  return {
+    roomsList: state.auth.rooms,
+  };
+}
+
+class RoomList extends Component {
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>RoomList</Text>
-      </View>
-    );
+    let { roomsList } = this.props;
+    if(!roomsList) {
+      console.log('roomsList vazia ');
+      return(
+        <View style={styles.container}>
+          <Text>
+            Loading rooms...
+          </Text>
+        </View>
+      );
+    } else {
+      console.log('roomsList preenchida = ', roomsList);
+      return (
+        <FlatList
+          data={roomsList}
+          renderItem={ ({item}) => <Text> {item.title} </Text> }
+
+        />
+      );
+    }
   }
 }
 const styles = StyleSheet.create({
@@ -28,3 +52,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#DCDCDC',
   }
 });
+
+export default connect(mapStateToProps)(RoomList);
